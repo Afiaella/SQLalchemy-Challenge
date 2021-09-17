428ad29fa7e3b5557d7ca3f47fd6f4d89fa8f452
Afiaella <emmanuela.osei.owusu@gmail.com>
2021-09-17 09:39:47 +0100

    sqlalchemy

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+@app.route("/")
+def welcome():
+    return(
+    f"/api/v1.0/precipitation"
+    f"/api/v1.0/stations"
+    f"/api/v1.0/tobs"
+    f"/api/v1.0/<start>"
+    f"/api/v1.0/<start>/<end>"
+    )
+
+    
+
+ 
